906ee843a3424dc46d6d57eb46fabb9158f5da4e
MrMuromec <ilia.d0@yandex.ru>
2015-04-17 18:22:59 +0400

    Размышления на парах
    сокращение кода (объеденение класса контроля с родиткльской формой)

--- Akupunctura/Akupunctura.Designer.cs
+++ Akupunctura/Akupunctura.Designer.cs
@@ -28,51 +28,51 @@
     /// </summary>
     private void InitializeComponent()
     {
-            this.MainMenu = new System.Windows.Forms.MenuStrip();
-            this.работаСПриборомToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.менюРаботыПрибораToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.MainMenu.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // MainMenu
-            // 
-            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+      this.MainMenu = new System.Windows.Forms.MenuStrip();
+      this.работаСПриборомToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.менюРаботыПрибораToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.MainMenu.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // MainMenu
+      // 
+      this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.работаСПриборомToolStripMenuItem});
-            this.MainMenu.Location = new System.Drawing.Point(0, 0);
-            this.MainMenu.Name = "MainMenu";
-            this.MainMenu.Size = new System.Drawing.Size(874, 24);
-            this.MainMenu.TabIndex = 0;
-            this.MainMenu.Text = "MainMenu";
-            // 
-            // работаСПриборомToolStripMenuItem
-            // 
-            this.работаСПриборомToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+      this.MainMenu.Location = new System.Drawing.Point(0, 0);
+      this.MainMenu.Name = "MainMenu";
+      this.MainMenu.Size = new System.Drawing.Size(874, 24);
+      this.MainMenu.TabIndex = 0;
+      this.MainMenu.Text = "MainMenu";
+      // 
+      // работаСПриборомToolStripMenuItem
+      // 
+      this.работаСПриборомToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.менюРаботыПрибораToolStripMenuItem});
-            this.работаСПриборомToolStripMenuItem.Name = "работаСПриборомToolStripMenuItem";
-            this.работаСПриборомToolStripMenuItem.Size = new System.Drawing.Size(127, 20);
-            this.работаСПриборомToolStripMenuItem.Text = "Работа с прибором";
-            // 
-            // менюРаботыПрибораToolStripMenuItem
-            // 
-            this.менюРаботыПрибораToolStripMenuItem.Name = "менюРаботыПрибораToolStripMenuItem";
-            this.менюРаботыПрибораToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
-            this.менюРаботыПрибораToolStripMenuItem.Text = "Меню работы прибора";
-            this.менюРаботыПрибораToolStripMenuItem.Click += new System.EventHandler(this.МенюРаботыПрибораToolStripMenuItem_Click);
-            // 
-            // Akupunctura
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(874, 476);
-            this.Controls.Add(this.MainMenu);
-            this.IsMdiContainer = true;
-            this.MainMenuStrip = this.MainMenu;
-            this.Name = "Akupunctura";
-            this.Text = "Akupunctura";
-            this.MainMenu.ResumeLayout(false);
-            this.MainMenu.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.работаСПриборомToolStripMenuItem.Name = "работаСПриборомToolStripMenuItem";
+      this.работаСПриборомToolStripMenuItem.Size = new System.Drawing.Size(127, 20);
+      this.работаСПриборомToolStripMenuItem.Text = "Работа с прибором";
+      // 
+      // менюРаботыПрибораToolStripMenuItem
+      // 
+      this.менюРаботыПрибораToolStripMenuItem.Name = "менюРаботыПрибораToolStripMenuItem";
+      this.менюРаботыПрибораToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+      this.менюРаботыПрибораToolStripMenuItem.Text = "Меню работы прибора";
+      this.менюРаботыПрибораToolStripMenuItem.Click += new System.EventHandler(this.МенюРаботыПрибораToolStripMenuItem_Click);
+      // 
+      // Akupunctura
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(874, 476);
+      this.Controls.Add(this.MainMenu);
+      this.IsMdiContainer = true;
+      this.MainMenuStrip = this.MainMenu;
+      this.Name = "Akupunctura";
+      this.Text = "Akupunctura";
+      this.MainMenu.ResumeLayout(false);
+      this.MainMenu.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
--- Akupunctura/Akupunctura.cs
+++ Akupunctura/Akupunctura.cs
@@ -8,21 +8,139 @@ using System.Text;
 using System.Windows.Forms;
 using Akupunctura.Logik;
 
+using System.Windows;
+using System.Threading;
+using System.IO.Ports;
+using System.IO;
+using System.Runtime.InteropServices;
+
 namespace Akupunctura
 {
   public partial class Akupunctura : Form
   {
-    public control_forms BD = new control_forms(); // инициализация рабочей базы и логики работы
+    // http://rsdn.ru/article/dotnet/CSThreading2.xml
+
+    private string address = Environment.CurrentDirectory + @"\БД"; // По началу там где лежит exe (Адрес папки)
+    private List<DateTime> ID_DOC = new List<DateTime>(); // id Докторов
+    public control_forms BD = new control_forms(); //  набор данных для форм и функции для работы с ними (возможно прощай)
 
     public Akupunctura()
     {  
       InitializeComponent();
-      BD.get_Parent(this);
     }
 
     private void МенюРаботыПрибораToolStripMenuItem_Click(object sender, EventArgs e)
     {
-        BD.form_Device01();
+        form_Device01();
     }
+    /********************************************************************************************/
+
+
+    /********************************************************************************************/
+    public List<DateTime> add_rows(string str) // Создание не совпадающего списка id 
+    {
+      DateTime id;
+      List<DateTime> many_id = new List<DateTime>();
+      for (int i = Directory.GetFiles(str, "*.txt").Length; i != 0; i--)
+      {
+        if (DateTime.TryParse(Path.GetFileNameWithoutExtension(Directory.GetFiles(str, "*.txt")[i - 1]).Replace(';', ':'), out id))
+          many_id.Add(id.ToUniversalTime());
+      }
+      return many_id;
+    }
+    /********************************************************************************************/
+    public void form_Device01() // Запуск работы с прибором
+    {
+      /*
+       * 2 аргумента
+       * входные данные - то с чем дают (могут и не дать)
+       * выходные - то что с чем работают (то что есть)
+       * проверка выдаёт праду когда данные корекны
+       * а если ложь то нужно запросить данные
+       */
+      while (!check_Position() && !check_Doctor() && !check_Patient()) ;
+     /*
+      if (check_Position() && check_Doctor() && check_Patient())
+      {
+        //Number = numbering(Parent);
+        Device01 device01 = new Device01(Parent, data_forms[Number - 1]);
+        device01.MdiParent = Parent;
+        device01.Show();
+      }
+    */
+    }
+    public void form_Position() // Запуск выбора бызы
+    {
+      /*
+      Position position = new Position(Parent.BD);
+      position.MdiParent = Parent;
+      position.Show();
+       * */
+    }
+    public void fofm_Doctor() // Запуск окна выбора врача
+    {
+      /*
+      //Number = numbering(Parent);
+      Authorization authorization = new Authorization(data_forms[Number - 1]);
+      authorization.MdiParent = Parent;
+      authorization.Show();
+       * */
+    }
+    public void fofm_Patient() // Запуск окна выбора пациента
+    {
+      /*
+      //Number = numbering(Parent);
+      Patient_list patient_list = new Patient_list(Parent, data_forms[Number - 1]);
+      patient_list.MdiParent = Parent;
+      patient_list.Show();
+       */
+    }
+    /****************************************************************************************/
+    public void get_Parent(Akupunctura mainForm) // Получение родителя
+    {
+      this.Parent = mainForm;
+    }
+    /****************************************************************************************/
+    private bool check_Position() // Проверка наличия базы
+    {
+      if (!Directory.GetDirectories(address.Replace(@"\БД", "")).Contains(address))
+      {
+        form_Position();
+        return false;
+      }
+      return true;
+    }
+    private bool check_Doctor() // Проверка наличия врача
+    {
+      /*
+      if (local_doctor.read_fio().Count() == 0)
+      {
+        fofm_Doctor();
+        return false;
+      }
+       * */
+      return true;
+    }
+    private bool check_Patient() // Проверка наличия пациента
+    {
+      /*
+      if (local_patient.read_fio().Count == 0)
+      {
+        fofm_Patient();
+        return false;
+      }
+       * */
+      return true;
+    }
+    /********************************************************************************************/
+    public void Addres(string address) // Получение адреса
+    {
+      this.address = address;
+    }
+    public string get_Addres() // Отдача адреса
+    {
+      return address;
+    }
+    /********************************************************************************************/
   }
 }
